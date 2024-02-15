feecb1c9ee2dee7a8c8ddb28ca3040baf372613b
Azfar Inan <azfar.inan0615@gmail.com>
2024-02-15 22:27:23 +0600

    feat: Created Splash Screen UI

---
+++ lib/src/core/widgets/button.dart
@@ -0,0 +1,123 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class Button extends StatelessWidget {
+  const Button({
+    Key? key,
+    required this.label,
+    required this.onPressed,
+    this.background,
+    this.textStyle,
+    this.height = 60,
+    this.width = double.infinity,
+    this.isLoading = false,
+    this.prefix,
+    this.disable = false,
+    this.borderRadius = 4,
+  }) : super(key: key);
+
+  final VoidCallback onPressed;
+  final String label;
+  final Color? background;
+  final TextStyle? textStyle;
+  final double height;
+  final double width;
+  final bool isLoading;
+  final Widget? prefix;
+  final bool disable;
+  final double borderRadius;
+
+  factory Button.filled({
+    required String label,
+    required VoidCallback onPressed,
+    Color? background,
+    TextStyle? textStyle,
+    double height = 60,
+    double width = double.infinity,
+    bool isLoading = false,
+    Widget? prefix,
+    bool disable = false,
+  }) {
+    return Button(
+      label: label,
+      onPressed: onPressed,
+      background: background,
+      textStyle: textStyle,
+      height: height,
+      width: width,
+      isLoading: isLoading,
+      prefix: prefix,
+      disable: disable,
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    double height = this.height;
+
+    return DecoratedBox(
+      decoration: disable
+          ? const BoxDecoration()
+          : BoxDecoration(
+              borderRadius: BorderRadius.circular(borderRadius),
+            ),
+      child: ElevatedButton(
+        onPressed: disable
+            ? null
+            : isLoading
+                ? null
+                : onPressed,
+        style: ElevatedButton.styleFrom(
+          elevation: 0,
+          padding: EdgeInsets.zero,
+          fixedSize: Size(width, height),
+          backgroundColor: background ?? Colors.teal,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(borderRadius),
+          ),
+        ),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            isLoading
+                ? Transform.scale(
+                    scale: 1,
+                    child: const SizedBox(
+                      height: 30,
+                      width: 30,
+                      child: CircularProgressIndicator(
+                        color: Colors.white,
+                      ),
+                    ),
+                  )
+                : prefix == null
+                    ? Text(
+                        label,
+                        style: textStyle ??
+                            GoogleFonts.montserrat(
+                              color: Colors.white,
+                              fontSize: 18,
+                              fontWeight: FontWeight.w600,
+                            ),
+                      )
+                    : Row(
+                        children: [
+                          prefix!,
+                          const SizedBox(width: 10),
+                          Text(
+                            label,
+                            style: textStyle ??
+                                GoogleFonts.archivo(
+                                  color: Colors.white,
+                                  fontSize: 14,
+                                  fontWeight: FontWeight.w500,
+                                ),
+                          ),
+                        ],
+                      ),
+          ],
+        ),
+      ),
+    );
+  }
+}
--- lib/src/feature/splash/presentation/views/splash_screen.dart
+++ lib/src/feature/splash/presentation/views/splash_screen.dart
@@ -1,13 +1,44 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:lottie/lottie.dart';
+import 'package:tr_store/src/core/widgets/button.dart';
 
 class SplashScreen extends StatelessWidget {
   const SplashScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text('Splash Screen'),
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+            const Spacer(),
+            Padding(
+              padding: const EdgeInsets.all(32.0),
+              child: Lottie.asset('assets/animations/splash_animation.json'),
+            ),
+            Text(
+              'TR Store',
+              textAlign: TextAlign.center,
+              style: GoogleFonts.montserrat(
+                fontSize: 32,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            const Spacer(),
+            Button(
+              onPressed: () {
+                // Navigator.pushNamed(context, WalletAppRoute.onBoardingScreen);
+              },
+              label: "Continue",
+              borderRadius: 50,
+            ),
+            const SizedBox(height: 16),
+          ],
+        ),
       ),
     );
   }
